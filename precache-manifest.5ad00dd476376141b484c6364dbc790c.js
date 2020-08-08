600d8fef3942ddbff060793e3ac2acdaa0a50c75
Nhat Ho <honhattan2411@gmail.com>
2020-08-08 14:32:50 -0700

    Updates

---
+++ precache-manifest.5ad00dd476376141b484c6364dbc790c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2ae2961409bcf18e1ff9d2c53338b0",
+    "url": "/React3_Monsters/index.html"
+  },
+  {
+    "revision": "d3d22fa3f7a5c9fff0a1",
+    "url": "/React3_Monsters/static/css/main.b2494342.chunk.css"
+  },
+  {
+    "revision": "5ba5ac096b0d3445fa8f",
+    "url": "/React3_Monsters/static/js/2.a8bb8d99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React3_Monsters/static/js/2.a8bb8d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d22fa3f7a5c9fff0a1",
+    "url": "/React3_Monsters/static/js/main.02ff5b12.chunk.js"
+  },
+  {
+    "revision": "c41b2e77909a664e8844",
+    "url": "/React3_Monsters/static/js/runtime-main.604559bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React3_Monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
